fe270b8a089430cde6614114e3ddd6de8fdb736d
amordgzmn <amor_deguzman@dlsu.edu.ph>
2025-03-05 18:43:58 +0800

    Add redirection to proper page with / route

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -2,6 +2,10 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get(`/`, (req, res) => {
+    res.redirect(`/guitar`);
+})
+
 router.get(`/guitar`, (req, res) => {
     res.render("mainpage", {
         cssfilename: "main.css",

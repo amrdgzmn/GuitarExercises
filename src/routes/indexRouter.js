126e592c0fb4605138d64e50af89a8449814c673
amordgzmn <amor_deguzman@dlsu.edu.ph>
2025-03-05 18:51:58 +0800

    Fix router syntax

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -3,10 +3,10 @@ const { Router } = require('express');
 const router = Router();
 
 router.get(`/`, (req, res) => {
-    res.redirect(`/guitar`);
+    res.redirect('/guitar');
 })
 
-router.get(`/guitar`, (req, res) => {
+router.get('/guitar', (req, res) => {
     res.render("mainpage", {
         cssfilename: "main.css",
         title: "Guitar Exercises Mini-Website",
